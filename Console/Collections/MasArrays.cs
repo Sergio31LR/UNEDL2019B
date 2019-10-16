dd163afa403bb9108c1797dcbfa10bb7065f8247
Sergio Alexis <sergioalexis31@hotmail.com>
2019-10-16 08:18:58 -0500

    colecciones

---
+++ Console/Collections/MasArrays.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class MasArrays
+    {
+public void Sorted()
+        {
+            SortedList sl = new SortedList();
+            sl.Add(1, "Valor 1");
+            sl.Add(5, "valor 5");
+            sl.Add(7,null);
+            ICollection llave = sl.Keys;
+            foreach (int k in llave)
+            {
+                Console.WriteLine("llave: "+k+"valor: "+sl[k]);
+            }
+        }
+    }
+}
---
+++ Console/Collections/UnArray.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class UnArray
+    {
+
+        public void a()
+        {
+            ArrayList al = new ArrayList();
+            al.Add(null);
+            al.Add("TO");
+        }
+    }
+}
